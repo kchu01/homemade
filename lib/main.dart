e627eafb13c5d5d737c339686a45d23312c31fe5
kchu01 <chukathy01@gmail.com>
2021-04-12 16:26:18 -0700

    Display an appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
+// import 'package:myapp/constants.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text('Project 4'))));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          // primarySwatch: Colors.blue.shade200,
+          ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stationary'),
+      ),
+    );
+  }
+}
