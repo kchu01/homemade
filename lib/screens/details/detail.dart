fca8ba980e9b02d1bebe15ce73146c3c431ffd92
kchu01 <chukathy01@gmail.com>
2021-04-14 13:51:50 -0700

    successfully load title and image by identifier

--- lib/screens/details/detail.dart
+++ lib/screens/details/detail.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
+import '../../models/recipe.dart';
 
 class Detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final recipes = Recipe.fetchAll();
+    final recipe = recipes.first;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('homemade'),
+        title: Text(recipe.name),
       ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ImageBanner("/images/pho.jpg"),
+            ImageBanner(recipe.imagePath),
             TextSection("Recipe name", "subtitle"),
             TextSection("Ingredients", "what you will need"),
             TextSection("Directions", "step 1: boil a large pot of water"),
