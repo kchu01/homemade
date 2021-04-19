e80591a6d4b0a23d848fe54d32477b2d3edaaf80
kchu01 <chukathy01@gmail.com>
2021-04-18 20:39:35 -0700

    add and edit recipe.dart descriptions

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -13,22 +13,32 @@ class Recipe {
       Recipe(1, 'Pho', 'images/pho.jpg', [
         RecipeDetail('Details',
             'A Vietnamese soup consisting of broth, rice noodles, herbs, and meat. Pho is considered the national dish of Vietnam.'),
+        RecipeDetail('Ingredients',
+            '- 1 large onion\n- 3 inch pieces of ginger\n-spices (star anise, whole cloves, cinnamon, cardamon, coriander)\n- 8 cups of beef broth\n- rock sugar\n- fish sauce'),
         RecipeDetail('Directions',
             'Step 1: boil a large pot of water.\nStep 2: Add spices to the pot.'),
       ]),
-      Recipe(2, 'Asada Tacos', 'images/taco.jpg', [
-        RecipeDetail('Details', 'Delicious asada tacos on corn tortillas.'),
+      Recipe(2, 'Carne Asada Tacos', 'images/taco.jpg', [
+        RecipeDetail('Details', 'Delicious skirt steak on corn tortillas.'),
+        RecipeDetail('Ingredients',
+            '- 2 lbs skirt steak\n- corn tortillas\n- cilantro and lime'),
         RecipeDetail(
             'Directions', 'Step 1: eat 100 tacos because they are so yummy.'),
       ]),
       Recipe(3, 'Ramen', 'images/ramen.jpg', [
-        RecipeDetail('Details', 'Warm relaxing bowl of ramen.'),
-        RecipeDetail('Directions', 'Step 1: just slurp.'),
+        RecipeDetail('Details',
+            'A Japanese noodle dish consisting of wheat noodles, meat or fish flavored broth, and toppings such as chashu.'),
+        RecipeDetail('Ingredients',
+            '- broth flavor of your choice\n- wheat noodles\n- chashu'),
+        RecipeDetail('Directions', 'Step 1: combine and slurp.'),
       ]),
       Recipe(4, 'Banh Xeo', 'images/banhxeo.jpg', [
-        RecipeDetail('Details', 'Flaky goodness.'),
+        RecipeDetail('Details',
+            'Crispy, flakey stuffed rice crepe that is popular in Vietnam.'),
+        RecipeDetail('Ingredients',
+            '- banh xeo flour pack with tumeric\n- toppings of your choice\n- fish sauce'),
         RecipeDetail('Directions',
-            'Step 1: realize you ate 3 already and contemplate eating another.'),
+            'Step 1: Pour a ladel of batter in the pan and wait for it to fry.\nStep 2: Add the toppings and fold the crepe in half\nStep 3: Pour fish sauce and enjoy!'),
       ]),
     ];
   }
